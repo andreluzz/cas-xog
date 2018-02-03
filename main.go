82f91513b338a11be56c2e0917e236fa4459136e
André Luiz Mendonça <andreluzz@gmail.com>
2018-02-03 06:17:26 -0200

    Version command-line argument

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"github.com/andreluzz/cas-xog/view"
+	"fmt"
+	"os"
+	"strings"
 )
 
-var version string
+var version = "Development"
 
 func main() {
+	if len(os.Args) > 1 {
+		arg := strings.ToLower(os.Args[1])
+		if strings.Contains(arg, "version") {
+			fmt.Printf("CAS-XOG version: %s\n", version)
+			return
+		}
+	}
+
 	view.Home(version)
 	var exit = false
 	for {
